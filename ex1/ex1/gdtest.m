7ca68f4d25481e1c8ed05e8070f179c1964a5dab
wanggang <wanggang3333@gmail.com>
2015-08-28 20:03:21 +0800

    finish ex1

---
+++ ex1/ex1/gdtest.m
@@ -0,0 +1,12 @@
+%% Initialization
+clear ; close all; clc
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+
+% Some gradient descent settings
+iterations = 1500;
+alpha = 0.01;
+theta = gradientDescent(X, y, theta, alpha, iterations);
